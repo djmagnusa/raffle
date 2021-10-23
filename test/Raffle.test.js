f302bbc295e04bd2512efb90a99a7fe18ab454d7
Pratush Bhandari <pratush.bh@gmail.com>
2021-10-23 15:16:30 +0530

    Testing ether is sent to the winner

--- test/Raffle.test.js
+++ test/Raffle.test.js
@@ -88,7 +88,24 @@ describe('Raffle Contract', () => {
         } catch(err) {
           assert(err);
         }
-    })
+    });
+
+    it('sends money to the winner and resets the players array', async() => {
+        await raffle.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('2', 'ether')
+        });
+
+        const initialBalance = await web3.eth.getBalance(accounts[0]);
+
+        await raffle.methods.pickWinner().send({ from: accounts[0] });
+
+        const finalBalance = await web3.eth.getBalance(accounts[0]);
+
+        const difference = finalBalance - initialBalance;
+        assert(difference > web3.utils.toWei('1.8', 'ether'));
+    });
+
 
    
     
